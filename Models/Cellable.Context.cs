a31e3be98c651c0cf9dc99c0c1c8d606b03d7869
Britt <1altaskier@gmail.com>
2020-01-20 18:36:40 -0500

    Database/Model change - Added Slide Show

--- Models/Cellable.Context.cs
+++ Models/Cellable.Context.cs
@@ -45,6 +45,7 @@ namespace CellableMVC.Models
         public virtual DbSet<DefectGroup> DefectGroups { get; set; }
         public virtual DbSet<VersionCapacity> VersionCapacities { get; set; }
         public virtual DbSet<SystemSetting> SystemSettings { get; set; }
+        public virtual DbSet<SlideShow> SlideShows { get; set; }
     
         public virtual ObjectResult<Order> GetOrderDetails(Nullable<int> userId)
         {
