754e437557e81be1009eb134d6dc3ea102452003
Britt <1altaskier@gmail.com>
2020-06-09 20:58:15 -0400

    Update MVC

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -354,7 +354,6 @@ namespace CellableMVC.Controllers
                     {
                         if (item.ToString().Contains("QuestionAnswer"))
                         {
-<<<<<<< HEAD
                             UserAnswer userAnswer = new UserAnswer();
 
                             string groupAnswerSession = Session["QuestionAnswer" + i].ToString();
@@ -370,18 +369,6 @@ namespace CellableMVC.Controllers
 
                             db.UserAnswers.Add(userAnswer);
                             db.SaveChanges();
-=======
-                            if (Session[item.ToString()].ToString() != "0.00" && Session[item.ToString()].ToString() != "0")
-                            {
-                                UserAnswer userAnswer = new UserAnswer();
-                                userAnswer.Answer = true;
-                                userAnswer.PossibleDefectId = int.Parse(item.ToString());
-                                userAnswer.UserPhoneId = userPhoneId;
-                                //userAnswer.DefectGroupId = 
-                                db.UserAnswers.Add(userAnswer);
-                                db.SaveChanges();
-                            }
->>>>>>> 28c30d10493d82fa3cb9c30c1ebd48d485dc6a94
                         }
                     }
 
